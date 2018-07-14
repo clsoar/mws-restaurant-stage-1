3d7bd3b0b5c9c09b0c44f1c550d7128a959829e1
CherieLee-Mason <clsoar@gmail.com>
2018-07-13 19:35:11 -0500

    Attempt 4 at github pages - images added to cache

--- sw.js
+++ sw.js
@@ -11,8 +11,17 @@ self.addEventListener('install', function(event) {
         '/css/styles.css',
         '/js/main.js',
         '/js/restaurant_info.js',
-        '/js/dbhelper.js'
-
+        '/js/dbhelper.js',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg'
       ]);
     })
   );
