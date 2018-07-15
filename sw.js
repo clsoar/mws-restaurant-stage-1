5442d5d50d6fb3f99ddd210e8bd30f409afd78c6
CherieLee-Mason <clsoar@gmail.com>
2018-07-14 21:30:27 -0500

    Added ignoreSearch option to match method

--- sw.js
+++ sw.js
@@ -48,8 +48,10 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
 
  event.respondWith(
-   caches.match(event.request).then(function(response) {
+   caches.match(event.request, {ignoreSearch : true}).then(function(response) {
      return response || fetch(event.request);
+   }).catch(function(error) {
+     console.log(error);
    })
  );
 });
